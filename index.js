3261837f30d1d9b2759f651baa1fd87eb23b4a73
Carlos Rodriguez <carlos@s8f.org>
2012-09-04 21:36:38 -0700

    implement stuff

---
+++ index.js
@@ -0,0 +1,23 @@
+var amino = require('amino');
+
+exports.name = 'amino';
+
+exports.dependencies = {
+  'http': '1.x'
+};
+
+exports.init = function (app, done) {
+  app.amino = amino.init(app.conf.get('amino'));
+  done();
+};
+
+exports.ready = function (app, done) {
+  var conf = app.conf.get('amino');
+  app.http.once('listening', done);
+  var service = conf.service;
+  if (!service) {
+    var pkgInfo = require(app.pkgPath);
+    service = pkgInfo.name + '@' + pkgInfo.version;
+  }
+  app.service = app.amino.createService(service, app.http);
+};
--- test/basic.js
+++ test/basic.js
@@ -1,3 +1,12 @@
+var cantina = require('cantina')
+  , plugin = require('../')
+
 describe('basic test', function () {
-  it('works');
+  it('works', function (done) {
+    var app = cantina.createApp(['http', plugin], function (err) {
+      assert(app.service.spec.host);
+      assert(app.service.spec.port);
+      done();
+    });
+  });
 });
