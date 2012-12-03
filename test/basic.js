bda5ce1caf768a391a17970537576d896ab9ea29
Carlos Rodriguez <carlos@s8f.org>
2012-12-03 11:01:24 -0800

    test non-core amino plugin

--- test/basic.js
+++ test/basic.js
@@ -9,12 +9,19 @@ describe('basic test', function () {
 
   it('works', function (done) {
     require(app.plugins.http);
+
+    app.on('amino:init', function (amino) {
+      amino.use(require('amino-queue'));
+    });
+
     require('../');
 
     app.conf.set('amino:silent', true);
 
     app.init(function (err) {
       assert.ifError(err);
+      assert(app.amino);
+      assert(app.amino.queue);
       assert(app.service.spec.host);
       assert(app.service.spec.port);
       done();
