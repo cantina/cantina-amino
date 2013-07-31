9d31f2b0219586ca95737f37b82b0b98d0eb83fa
Brian Link <cpsubrian@gmail.com>
2013-07-30 17:31:18 -0700

    Allow service (and app.server) to be optional.

--- cantina-amino.js
+++ cantina-amino.js
@@ -2,9 +2,6 @@ var app = require('cantina')
   , amino = require('amino')
   , conf;
 
-// Requires a server (usually provided by cantina-web).
-if (!app.server || !app.server.listen) throw new Error('No server found on app.server.');
-
 // Default conf.
 app.conf.add({
   amino: {
@@ -23,11 +20,13 @@ if (conf) {
   app.amino = amino.init(conf);
 
   // Create service when app is started.
-  app.hook('start').add(function (done) {
-    app.server.once('listening', function () {
-      app.log(app.service.spec + ' started');
-      done();
+  if (app.server && conf.service) {
+    app.hook('start').add(function (done) {
+      app.server.once('listening', function () {
+        app.log(app.service.spec + ' started');
+        done();
+      });
+      app.service = app.amino.createService(conf.service.name + '@' + conf.service.version, app.server);
     });
-    app.service = app.amino.createService(conf.service.name + '@' + conf.service.version, app.server);
-  });
+  }
 }
