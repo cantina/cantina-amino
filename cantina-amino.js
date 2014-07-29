8e6102899ee997f39e0c8b82cca587db62aa7835
Brian Link <cpsubrian@gmail.com>
2014-07-28 18:29:33 -0700

    Updates to cantina 4.1.x

--- cantina-amino.js
+++ cantina-amino.js
@@ -1,32 +1,33 @@
-var app = require('cantina')
-  , amino = require('amino')
+var amino = require('amino')
   , conf;
 
-// Default conf.
-app.conf.add({
-  amino: {
-    service: {
-      name: app.pkgData ? app.pkgData.name : 'app',
-      version: app.pkgData ? app.pkgData.version : '0.0.0'
+module.exports = function (app) {
+  // Default conf.
+  app.conf.add({
+    amino: {
+      service: {
+        name: app.pkgData ? app.pkgData.name : 'app',
+        version: app.pkgData ? app.pkgData.version : '0.0.0'
+      }
+    }
+  });
+
+  // Get conf.
+  conf = app.conf.get('amino');
+
+  // Init amino.
+  if (conf) {
+    app.amino = amino.init(conf);
+
+    // Create service when app is started.
+    if (app.server && conf.service) {
+      app.hook('start').add(function (done) {
+        app.server.once('listening', function () {
+          app.log(app.service.spec + ' started');
+          done();
+        });
+        app.service = app.amino.createService(conf.service.name + '@' + conf.service.version, app.server);
+      });
     }
   }
-});
-
-// Get conf.
-conf = app.conf.get('amino');
-
-// Init amino.
-if (conf) {
-  app.amino = amino.init(conf);
-
-  // Create service when app is started.
-  if (app.server && conf.service) {
-    app.hook('start').add(function (done) {
-      app.server.once('listening', function () {
-        app.log(app.service.spec + ' started');
-        done();
-      });
-      app.service = app.amino.createService(conf.service.name + '@' + conf.service.version, app.server);
-    });
-  }
-}
+};
--- test/basic.js
+++ test/basic.js
@@ -1,16 +1,16 @@
-var app = require('cantina')
-  , assert = require('assert')
+var assert = require('assert')
   , util = require('util')
   , http = require('http');
 
 describe('basic test', function () {
   before(function (done) {
+    app = require('cantina').createApp();
     app.silence();
     app.boot(function (err) {
       assert.ifError(err);
 
       app.server = http.createServer();
-      require('../');
+      app.require('../');
       app.amino.use(require('amino-queue'));
 
       app.start(done);
